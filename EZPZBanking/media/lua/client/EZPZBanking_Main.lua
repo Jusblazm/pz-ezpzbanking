4e59cc6fef3e2f4a8b2388daa4e987297be15d2b
Jusblazm <ryanlarkin0@gmail.com>
2026-02-15 14:51:26 -0800

    v1.4 - updated korean translations, legacy version warning added

--- EZPZBanking/media/lua/client/EZPZBanking_Main.lua
+++ EZPZBanking/media/lua/client/EZPZBanking_Main.lua
@@ -32,7 +32,7 @@ end
 Events.OnKeyPressed.Add(onGlobalKeyPressed)
 
 local function updateWarning(playerIndex, playerObj)
-    print("[EZPZBanking] Warning: This version of EZPZ Banking is no longer supported. Please update to the newest version to get new features and continued support")
+    print("[EZPZBanking] Warning: This version of EZPZ Banking is no longer supported. Please update to the newest version to get new features and continued support.")
 end
 
 Events.OnCreatePlayer.Add(updateWarning)
