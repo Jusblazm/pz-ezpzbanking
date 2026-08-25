0d53c47d4064ed0606f6cc91c880dd88353b31d5
Jusblazm <ryanlarkin0@gmail.com>
2026-08-25 12:33:18 -0700

    v2.0.6 - temporarily disabled admin panel (#10)

--- EZPZBanking/42.20/media/lua/client/ISUI/AdminPanel/EZPZBanking_ISAdminPanelUI.lua
+++ EZPZBanking/42.20/media/lua/client/ISUI/AdminPanel/EZPZBanking_ISAdminPanelUI.lua
@@ -1,32 +1,32 @@
--- EZPZBanking_ISAdminPanelUI
-local originalCreate = ISAdminPanelUI.create
+-- -- EZPZBanking_ISAdminPanelUI
+-- local originalCreate = ISAdminPanelUI.create
 
-function ISAdminPanelUI:create()
-    originalCreate(self)
+-- function ISAdminPanelUI:create()
+--     originalCreate(self)
 
-    local btnWid = 200
-    local btnHgt = getTextManager():getFontHeight(UIFont.Small) + 6
-    local padding = 10
-    local x = padding + 1
-    local y = getTextManager():getFontHeight(UIFont.Medium) + padding * 2 + 1
+--     local btnWid = 200
+--     local btnHgt = getTextManager():getFontHeight(UIFont.Small) + 6
+--     local padding = 10
+--     local x = padding + 1
+--     local y = getTextManager():getFontHeight(UIFont.Medium) + padding * 2 + 1
 
-    self.ezpzbankingAdminBtn = ISButton:new(x, y, btnWid, btnHgt, getText("UI_EZPZBanking_ISEZPZBankingAdminUI_AdminPanelButton"), self, ISAdminPanelUI.onOptionMouseDown)
+--     self.ezpzbankingAdminBtn = ISButton:new(x, y, btnWid, btnHgt, getText("UI_EZPZBanking_ISEZPZBankingAdminUI_AdminPanelButton"), self, ISAdminPanelUI.onOptionMouseDown)
 
-    self.ezpzbankingAdminBtn.internal = "EZPZBANKING"
-    self.ezpzbankingAdminBtn:initialise()
-    self.ezpzbankingAdminBtn:instantiate()
-    self.ezpzbankingAdminBtn.borderColor = self.buttonBorderColor
-    self.ezpzbankingAdminBtn.tooltip = getText("Tooltip_EZPZBanking_AdminPanelUI_AdminPanelButton")
+--     self.ezpzbankingAdminBtn.internal = "EZPZBANKING"
+--     self.ezpzbankingAdminBtn:initialise()
+--     self.ezpzbankingAdminBtn:instantiate()
+--     self.ezpzbankingAdminBtn.borderColor = self.buttonBorderColor
+--     self.ezpzbankingAdminBtn.tooltip = getText("Tooltip_EZPZBanking_AdminPanelUI_AdminPanelButton")
 
-    self:addChild(self.ezpzbankingAdminBtn)
-end
+--     self:addChild(self.ezpzbankingAdminBtn)
+-- end
 
-local originalMouseDown = ISAdminPanelUI.onOptionMouseDown
+-- local originalMouseDown = ISAdminPanelUI.onOptionMouseDown
 
-function ISAdminPanelUI:onOptionMouseDown(button, x, y)
-    if button.internal == "EZPZBANKING" then
-        EZPZBanking_ISEZPZBankingAdminUI.openAdminUI()
-    end
+-- function ISAdminPanelUI:onOptionMouseDown(button, x, y)
+--     if button.internal == "EZPZBANKING" then
+--         EZPZBanking_ISEZPZBankingAdminUI.openAdminUI()
+--     end
 
-    originalMouseDown(self, button, x, y)
-end
+--     originalMouseDown(self, button, x, y)
+-- end
